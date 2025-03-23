ccab9bdec95ec4808f6adb57ed37f3e7fc5aec29
Ромась Юрій <romas_yura@ukr.net>
2025-03-23 23:01:16 +0200

    Feature(api) : added FastAPI lib and POST func

--- bot.py
+++ bot.py
@@ -1,16 +1,37 @@
 import discord
+import requests
+import asyncio
+import uvicorn
 from dotenv import dotenv_values
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 
 config = dotenv_values("config.env")
 
+app = FastAPI()
+
 TOKEN = config.get("TOKEN")
+
+class Message(BaseModel):
+    chat_id: int
+    text: str
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+
+@app.post("/send-to-discord/")
+async def send_to_discord(message: Message):
+    channel = client.get_channel(message.chat_id)
+    if channel:
+        await channel.send(message.text)
+        return {"status": "sent"}
+    return {"error": "Channel not found"}
 
 @client.event
 async def on_ready():
@@ -21,7 +42,6 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # Виводимо повідомлення, якщо воно містить текст
     if message.content.strip():
         print(f"[{message.guild.name} | {message.channel.name}] {message.author}: {message.content}")
 
@@ -30,4 +50,17 @@ async def on_message(message):
         if message.content.strip():
             file_bytes = await attachment.read()
 
-client.run(TOKEN)
+async def main():
+    # Запускаємо FastAPI у фоновому режимі
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000, log_level="info")
+    server = uvicorn.Server(config)
+
+    # Запускаємо обидва сервіси одночасно
+    await asyncio.gather(
+        server.serve(),
+        client.start(TOKEN)
+    )
+
+asyncio.run(main())
+
+#client.run(TOKEN)
