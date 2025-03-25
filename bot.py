c88c1d9f050288d37c536ddf75cf578e1cbb1fda
Ромась Юрій <romas_yura@ukr.net>
2025-03-25 15:29:43 +0200

    feature(api) : request to external api
    split methods to post/get/delete

--- bot.py
+++ bot.py
@@ -1,22 +1,29 @@
 import discord
-import requests
 import asyncio
 import uvicorn
+import aiohttp
 from dotenv import dotenv_values
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List, Literal
 
 
 
-config = dotenv_values("config.env")
+cfg = dotenv_values("config.env")
 
 app = FastAPI()
 
-TOKEN = config.get("TOKEN")
+TOKEN = cfg.get("TOKEN")
+TARGET_API_URL = cfg.get("TARGET_API_URL")
 
 class Message(BaseModel):
-    chat_id: int
+    chat_id : int
     text: str
+
+class DELETE(BaseModel):
+    chat_id : int
+    message_id: int
+
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -25,13 +32,40 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-@app.post("/send-to-discord/")
-async def send_to_discord(message: Message):
+
+
+@app.post("/discord/")
+async def send_to_api(message: Message):
+    """Відправляє запит на інше API"""
+    async with aiohttp.ClientSession() as session:
+        payload = {"chat_id": message.chat_id, "text": message.text}
+        async with session.get(TARGET_API_URL, json=payload) as response:
+            result = await response.json()
+            return {"status": "sent", "response": result}
+
+@app.get("/discord/")
+async def send_to_discord(message:Message):
     channel = client.get_channel(message.chat_id)
     if channel:
         await channel.send(message.text)
         return {"status": "sent"}
     return {"error": "Channel not found"}
+
+@app.delete("/discord/")
+async def delete_message(message: DELETE):
+    """Видаляє повідомлення за ID"""
+    channel = client.get_channel(message.chat_id)
+    try:
+        message = await channel.fetch_message(message.message_id)
+        await message.delete()
+        print("✅ Повідомлення видалено!")
+    except discord.NotFound:
+        print("❌ Повідомлення не знайдено!")
+    except discord.Forbidden:
+        print("❌ У мене немає прав на видалення!")
+    except discord.HTTPException:
+        print("❌ Сталася помилка при видаленні!")
+
 
 @client.event
 async def on_ready():
@@ -47,20 +81,18 @@ async def on_message(message):
 
     for attachment in message.attachments:
         print(f"📂 Отримано файл: {attachment.filename} ({attachment.size} байт)\n")
-        if message.content.strip():
-            file_bytes = await attachment.read()
+        #if message.content.strip():
+        #    file_bytes = await attachment.read()
 
 async def main():
     # Запускаємо FastAPI у фоновому режимі
     config = uvicorn.Config(app, host="0.0.0.0", port=8000, log_level="info")
     server = uvicorn.Server(config)
 
-    # Запускаємо обидва сервіси одночасно
+
     await asyncio.gather(
         server.serve(),
         client.start(TOKEN)
     )
 
 asyncio.run(main())
-
-#client.run(TOKEN)
